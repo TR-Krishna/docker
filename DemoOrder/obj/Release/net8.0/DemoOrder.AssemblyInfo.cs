7c85a997d3d73d456c3ad6d9a0cb930eacd6c5dd
TR-Krishna <trkrishna2004@gmail.com>
2026-06-04 11:22:16 +0530

    added deployment - using github actions

--- DemoOrder/obj/Release/net8.0/DemoOrder.AssemblyInfo.cs
+++ DemoOrder/obj/Release/net8.0/DemoOrder.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoOrder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5168eb559b8d786a7360383d0feccb9bcc97d2bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoOrder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoOrder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
