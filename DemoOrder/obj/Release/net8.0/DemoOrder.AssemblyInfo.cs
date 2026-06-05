419d859e3bb37dbcb216349c5c1f7635f6b42efb
TR-Krishna <trkrishna2004@gmail.com>
2026-06-05 09:48:51 +0530

    trigger+swagger push

--- DemoOrder/obj/Release/net8.0/DemoOrder.AssemblyInfo.cs
+++ DemoOrder/obj/Release/net8.0/DemoOrder.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoOrder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5ce0f63a48161f99494b79a5a1436418319d076")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06542a4cbf33571f8d7b1451cc41b9729236e621")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoOrder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoOrder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
